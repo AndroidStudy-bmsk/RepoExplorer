cee61ad49be1f217971b5e6c2078d93bc2ac3f60
bmsk <vv99911@gmail.com>
2023-04-24 23:25:59 +0900

    Update MainActivity.kt

--- app/src/main/java/org/bmsk/android_network_2/MainActivity.kt
+++ app/src/main/java/org/bmsk/android_network_2/MainActivity.kt
@@ -1,8 +1,11 @@
 package org.bmsk.android_network_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.bmsk.android_network_2.adapter.UserAdapter
+import org.bmsk.android_network_2.databinding.ActivityMainBinding
 import org.bmsk.android_network_2.model.Repo
 import org.bmsk.android_network_2.model.UserDTO
 import org.bmsk.android_network_2.network.GitHubService
@@ -13,9 +16,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create()).build()
@@ -31,15 +37,23 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        val userAdapter = UserAdapter()
+
+        val recyclerView = binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
         gitHubService.getSearchUsers("squar").enqueue(object: Callback<UserDTO>{
             override fun onResponse(call: Call<UserDTO>, response: Response<UserDTO>) {
                 Log.d("MainActivity", "Search User: ${response.body().toString()}")
+
+                userAdapter.submitList(response.body()?.items)
             }
 
             override fun onFailure(call: Call<UserDTO>, t: Throwable) {
 
             }
-
         })
     }
 }
